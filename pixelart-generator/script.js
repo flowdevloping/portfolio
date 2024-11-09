a2d5cb88d4b455e0d2495e3d00889f014a6f2963
Florian Purschke <flowtronicx@gmail.com>
2024-11-09 15:11:10 +0100

    style things

--- pixelart-generator/script.js
+++ pixelart-generator/script.js
@@ -9,6 +9,7 @@ const createBtn = document.getElementById('create_btn');
 // Function which creates grid with pixels
 function populate(size, event) {
     pxl_container.innerHTML = "";
+    pxl_container.style.backgroundColor = 'white';
     // Updating the --size CSS variable
     pxl_container.style.setProperty('--size', size);
 
